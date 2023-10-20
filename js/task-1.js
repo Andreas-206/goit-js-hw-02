2a45bffb87faeb12824ddceac019e811dc6c612f
Andreas-206 <puma_real@ukr.net>
2023-10-20 14:07:37 +0300

    task-1 finish

--- js/task-1.js
+++ js/task-1.js
@@ -1,9 +1,9 @@
 function makeTransaction(quantity, pricePerDroid, customerCredits) {
 const totalPrice = quantity * pricePerDroid;
-if(){
-
-}else {
-    
+if(totalPrice > customerCredits){ 
+return "Insufficient funds!";
+}else { 
+return `You ordered ${quantity} droids worth ${totalPrice} credits`;
 }
 }
 
